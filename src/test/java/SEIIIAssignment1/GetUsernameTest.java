0cd28293e636cfb7ecc778f2e02b34de4fd1418c
AngryGodoter <Y.Jiang3@nuigalway.ie>
2022-09-26 15:41:34 +0100

    Fixed unit test code.

--- src/test/java/SEIIIAssignment1/GetUsernameTest.java
+++ src/test/java/SEIIIAssignment1/GetUsernameTest.java
@@ -1,6 +1,9 @@
 package SEIIIAssignment1;
 
 import org.joda.time.DateTime;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /***
  * This class is used to perform some simple testings with the get username method.
@@ -8,27 +11,49 @@ import org.joda.time.DateTime;
  *
  */
 public class GetUsernameTest {
-
-	public static void main(String[] args)
+	
+	//create a lecturer
+	Lecturer l1 = new Lecturer("BakeFenney", 42, new DateTime("1980-09-22"), 1, null);
+	//create some students
+	Student s1 = new Student("DavidSwain", 20, new DateTime("2002-06-02"), 1, null, null);
+	Student s2 = new Student("KarlBuck", 21, new DateTime("2001-11-03"), 2, null, null);
+	//create a course
+	Course c1 = new Course("CT", null, null, new DateTime("2019-09-22"), new DateTime("2023-05-01"));
+	//create a module
+	Module m1 = new Module("Simple AI", "CT999", null, null, l1);
+	
+	@Test
+	public void testStudentGetname1()
 	{
-		//create some students
-		Student s1 = new Student("DavidSwain", 20, new DateTime("2002-06-02"), 1, null, null);
-		Student s2 = new Student("KarlBuck", 21, new DateTime("2001-11-03"), 2, null, null);
-		//create a lecturer
-		Lecturer l1 = new Lecturer("BakeFenney", 42, new DateTime("1980-09-22"), 1, null);
-		
-		//test the getUsername method
-		System.out.println("The first student is called: " + s1.getName());
-		System.out.println("He is " + Integer.toString(s1.getAge()) + " years old");
-		System.out.println("His username is: " + s1.getUsername());
-		System.out.println("\n\n");
-		System.out.println("The second student is called: " + s2.getName());
-		System.out.println("He is " + Integer.toString(s2.getAge()) + " years old");
-		System.out.println("His username is: " + s2.getUsername());
-		System.out.println("\n\n");
-		System.out.println("The Lecturer is called: " + l1.getName());
-		System.out.println("He is " + Integer.toString(l1.getAge()) + " years old");
-		System.out.println("His username is: " + l1.getUsername());
+		String s1username = s1.getUsername();
+		assertEquals("DavidSwain20", s1username);
 	}
 	
+	@Test
+	public void testStudentGetname2()
+	{
+		String s2username = s2.getUsername();
+		assertEquals("KarlBuck21", s2username);
+	}
+	
+	@Test
+	public void testLecturerGetname()
+	{
+		String l1username = l1.getUsername();
+		assertEquals("BakeFenney42", l1username);
+	}
+	
+	@Test
+	public void testCourseGetName()
+	{
+		String courseCode = c1.getName();
+		assertEquals("CT", courseCode);
+	}
+	
+	@Test
+	public void testModuleGetName()
+	{
+		String moduleName = m1.getName();
+		assertEquals("Simple AI", moduleName);
+	}
 }
